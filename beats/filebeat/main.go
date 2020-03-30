242f596c8fa901f19355ade9fcdc0f4512e9447a
Renato Torres <renato.torres@outsystems.com>
2020-03-30 09:05:12 +0100

    Add support to filebeat

---
+++ beats/filebeat/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+
+	"github.com/elastic/beats/filebeat/cmd"
+	_ "github.com/renato0307/sqs_beats/outputs/sqs"
+)
+
+func main() {
+	if err := cmd.RootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
